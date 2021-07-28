8923d9668248974fc356471267282443bb6bcd93
Felipe Rieth <36487969+FelipeRieth@users.noreply.github.com>
2021-07-28 15:18:43 -0300

    Alpha 0.02v
    Working On Android.

--- lib/pages/video_pages.dart
+++ lib/pages/video_pages.dart
@@ -20,329 +20,41 @@ class _VideoPagesState extends State<VideoPages> {
 
                 Card(
                   child: ListTile(
-                    title: Text('Temporada 1',textAlign: TextAlign.center),
+                    title: Text('Bee ',textAlign: TextAlign.center),
                     subtitle: Text(
-                        'Os Simpsons '
+                        'MP4 Short Video '
                     ,textAlign: TextAlign.center,),
-                    leading: CircleAvatar(backgroundImage: NetworkImage('https://media.fstatic.com/lKCjH5bY9N2lqLxX3JzhEnw_wFk=/290x478/smart/media/movies/covers/2019/08/2019-08-07_1.png')),
+                    leading: CircleAvatar(backgroundImage: NetworkImage('https://upload.wikimedia.org/wikipedia/commons/4/4d/Apis_mellifera_Western_honey_bee.jpg')),
                     onTap: () async {
 
 
                       var navigationResult= await Navigator.push(context, new MaterialPageRoute(builder: (context) =>
-                        ChewieListItem(videoPlayerController: VideoPlayerController.network('https://videotemplate.lazyclique.com.br/Videos/Simpsons/ss.mp4'),looping: true)));
+                        ChewieListItem(videoPlayerController: VideoPlayerController.network('https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4'),looping: true)));
 
                     },
                     trailing: CircleAvatar(child: Icon(Icons.forward),),
 
                   ),
                 ),
+
                 Card(
                   child: ListTile(
-                    title: Text('Temporada 1',textAlign: TextAlign.center),
+                    title: Text('Puppies',textAlign: TextAlign.center),
                     subtitle: Text(
-                      'Os Simpsons '
+                      'Two little dogs'
                       ,textAlign: TextAlign.center,),
-                    leading: CircleAvatar(backgroundImage: NetworkImage('https://media.fstatic.com/lKCjH5bY9N2lqLxX3JzhEnw_wFk=/290x478/smart/media/movies/covers/2019/08/2019-08-07_1.png')),
+                    leading: CircleAvatar(backgroundImage: NetworkImage('https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2016/05/19091354/Weimaraner-puppy-outdoors-with-bright-blue-eyes.20190813165758508-1.jpg')),
                     onTap: () async {
 
 
                       var navigationResult= await Navigator.push(context, new MaterialPageRoute(builder: (context) =>
-                          ChewieListItem(videoPlayerController: VideoPlayerController.network('https://videotemplate.lazyclique.com.br/Videos/Simpsons/ss.mp4'),looping: true)));
+                          ChewieListItem(videoPlayerController: VideoPlayerController.network('https://static.videezy.com/system/resources/previews/000/037/964/original/baby_dog13.mp4'),looping: true)));
 
                     },
                     trailing: CircleAvatar(child: Icon(Icons.forward),),
 
                   ),
                 ),
-                Card(
-                  child: ListTile(
-                    title: Text('Temporada 1',textAlign: TextAlign.center),
-                    subtitle: Text(
-                      'Os Simpsons '
-                      ,textAlign: TextAlign.center,),
-                    leading: CircleAvatar(backgroundImage: NetworkImage('https://media.fstatic.com/lKCjH5bY9N2lqLxX3JzhEnw_wFk=/290x478/smart/media/movies/covers/2019/08/2019-08-07_1.png')),
-                    onTap: () async {
-
-
-                      var navigationResult= await Navigator.push(context, new MaterialPageRoute(builder: (context) =>
-                          ChewieListItem(videoPlayerController: VideoPlayerController.network('https://videotemplate.lazyclique.com.br/Videos/Simpsons/ss.mp4'),looping: true)));
-
-                    },
-                    trailing: CircleAvatar(child: Icon(Icons.forward),),
-
-                  ),
-                ),
-                Card(
-                  child: ListTile(
-                    title: Text('Temporada 1',textAlign: TextAlign.center),
-                    subtitle: Text(
-                      'Os Simpsons '
-                      ,textAlign: TextAlign.center,),
-                    leading: CircleAvatar(backgroundImage: NetworkImage('https://media.fstatic.com/lKCjH5bY9N2lqLxX3JzhEnw_wFk=/290x478/smart/media/movies/covers/2019/08/2019-08-07_1.png')),
-                    onTap: () async {
-
-
-                      var navigationResult= await Navigator.push(context, new MaterialPageRoute(builder: (context) =>
-                          ChewieListItem(videoPlayerController: VideoPlayerController.network('https://videotemplate.lazyclique.com.br/Videos/Simpsons/ss.mp4'),looping: true)));
-
-                    },
-                    trailing: CircleAvatar(child: Icon(Icons.forward),),
-
-                  ),
-                ),
-                Card(
-                  child: ListTile(
-                    title: Text('Temporada 1',textAlign: TextAlign.center),
-                    subtitle: Text(
-                      'Os Simpsons '
-                      ,textAlign: TextAlign.center,),
-                    leading: CircleAvatar(backgroundImage: NetworkImage('https://media.fstatic.com/lKCjH5bY9N2lqLxX3JzhEnw_wFk=/290x478/smart/media/movies/covers/2019/08/2019-08-07_1.png')),
-                    onTap: () async {
-
-
-                      var navigationResult= await Navigator.push(context, new MaterialPageRoute(builder: (context) =>
-                          ChewieListItem(videoPlayerController: VideoPlayerController.network('https://videotemplate.lazyclique.com.br/Videos/Simpsons/ss.mp4'),looping: true)));
-
-                    },
-                    trailing: CircleAvatar(child: Icon(Icons.forward),),
-
-                  ),
-                ),
-                Card(
-                  child: ListTile(
-                    title: Text('Temporada 1',textAlign: TextAlign.center),
-                    subtitle: Text(
-                      'Os Simpsons '
-                      ,textAlign: TextAlign.center,),
-                    leading: CircleAvatar(backgroundImage: NetworkImage('https://media.fstatic.com/lKCjH5bY9N2lqLxX3JzhEnw_wFk=/290x478/smart/media/movies/covers/2019/08/2019-08-07_1.png')),
-                    onTap: () async {
-
-
-                      var navigationResult= await Navigator.push(context, new MaterialPageRoute(builder: (context) =>
-                          ChewieListItem(videoPlayerController: VideoPlayerController.network('https://videotemplate.lazyclique.com.br/Videos/Simpsons/ss.mp4'),looping: true)));
-
-                    },
-                    trailing: CircleAvatar(child: Icon(Icons.forward),),
-
-                  ),
-                ),
-                Card(
-                  child: ListTile(
-                    title: Text('Temporada 1',textAlign: TextAlign.center),
-                    subtitle: Text(
-                      'Os Simpsons '
-                      ,textAlign: TextAlign.center,),
-                    leading: CircleAvatar(backgroundImage: NetworkImage('https://media.fstatic.com/lKCjH5bY9N2lqLxX3JzhEnw_wFk=/290x478/smart/media/movies/covers/2019/08/2019-08-07_1.png')),
-                    onTap: () async {
-
-
-                      var navigationResult= await Navigator.push(context, new MaterialPageRoute(builder: (context) =>
-                          ChewieListItem(videoPlayerController: VideoPlayerController.network('https://videotemplate.lazyclique.com.br/Videos/Simpsons/ss.mp4'),looping: true)));
-
-                    },
-                    trailing: CircleAvatar(child: Icon(Icons.forward),),
-
-                  ),
-                ),
-                Card(
-                  child: ListTile(
-                    title: Text('Temporada 1',textAlign: TextAlign.center),
-                    subtitle: Text(
-                      'Os Simpsons '
-                      ,textAlign: TextAlign.center,),
-                    leading: CircleAvatar(backgroundImage: NetworkImage('https://media.fstatic.com/lKCjH5bY9N2lqLxX3JzhEnw_wFk=/290x478/smart/media/movies/covers/2019/08/2019-08-07_1.png')),
-                    onTap: () async {
-
-
-                      var navigationResult= await Navigator.push(context, new MaterialPageRoute(builder: (context) =>
-                          ChewieListItem(videoPlayerController: VideoPlayerController.network('https://videotemplate.lazyclique.com.br/Videos/Simpsons/ss.mp4'),looping: true)));
-
-                    },
-                    trailing: CircleAvatar(child: Icon(Icons.forward),),
-
-                  ),
-                ),
-                Card(
-                  child: ListTile(
-                    title: Text('Temporada 1',textAlign: TextAlign.center),
-                    subtitle: Text(
-                      'Os Simpsons '
-                      ,textAlign: TextAlign.center,),
-                    leading: CircleAvatar(backgroundImage: NetworkImage('https://media.fstatic.com/lKCjH5bY9N2lqLxX3JzhEnw_wFk=/290x478/smart/media/movies/covers/2019/08/2019-08-07_1.png')),
-                    onTap: () async {
-
-
-                      var navigationResult= await Navigator.push(context, new MaterialPageRoute(builder: (context) =>
-                          ChewieListItem(videoPlayerController: VideoPlayerController.network('https://videotemplate.lazyclique.com.br/Videos/Simpsons/ss.mp4'),looping: true)));
-
-                    },
-                    trailing: CircleAvatar(child: Icon(Icons.forward),),
-
-                  ),
-                ),
-                Card(
-                  child: ListTile(
-                    title: Text('Temporada 1',textAlign: TextAlign.center),
-                    subtitle: Text(
-                      'Os Simpsons '
-                      ,textAlign: TextAlign.center,),
-                    leading: CircleAvatar(backgroundImage: NetworkImage('https://media.fstatic.com/lKCjH5bY9N2lqLxX3JzhEnw_wFk=/290x478/smart/media/movies/covers/2019/08/2019-08-07_1.png')),
-                    onTap: () async {
-
-
-                      var navigationResult= await Navigator.push(context, new MaterialPageRoute(builder: (context) =>
-                          ChewieListItem(videoPlayerController: VideoPlayerController.network('https://videotemplate.lazyclique.com.br/Videos/Simpsons/ss.mp4'),looping: true)));
-
-                    },
-                    trailing: CircleAvatar(child: Icon(Icons.forward),),
-
-                  ),
-                ),
-                Card(
-                  child: ListTile(
-                    title: Text('Temporada 1',textAlign: TextAlign.center),
-                    subtitle: Text(
-                      'Os Simpsons '
-                      ,textAlign: TextAlign.center,),
-                    leading: CircleAvatar(backgroundImage: NetworkImage('https://media.fstatic.com/lKCjH5bY9N2lqLxX3JzhEnw_wFk=/290x478/smart/media/movies/covers/2019/08/2019-08-07_1.png')),
-                    onTap: () async {
-
-
-                      var navigationResult= await Navigator.push(context, new MaterialPageRoute(builder: (context) =>
-                          ChewieListItem(videoPlayerController: VideoPlayerController.network('https://videotemplate.lazyclique.com.br/Videos/Simpsons/ss.mp4'),looping: true)));
-
-                    },
-                    trailing: CircleAvatar(child: Icon(Icons.forward),),
-
-                  ),
-                ),
-                Card(
-                  child: ListTile(
-                    title: Text('Temporada 1',textAlign: TextAlign.center),
-                    subtitle: Text(
-                      'Os Simpsons '
-                      ,textAlign: TextAlign.center,),
-                    leading: CircleAvatar(backgroundImage: NetworkImage('https://media.fstatic.com/lKCjH5bY9N2lqLxX3JzhEnw_wFk=/290x478/smart/media/movies/covers/2019/08/2019-08-07_1.png')),
-                    onTap: () async {
-
-
-                      var navigationResult= await Navigator.push(context, new MaterialPageRoute(builder: (context) =>
-                          ChewieListItem(videoPlayerController: VideoPlayerController.network('https://videotemplate.lazyclique.com.br/Videos/Simpsons/ss.mp4'),looping: true)));
-
-                    },
-                    trailing: CircleAvatar(child: Icon(Icons.forward),),
-
-                  ),
-                ),
-                Card(
-                  child: ListTile(
-                    title: Text('Temporada 1',textAlign: TextAlign.center),
-                    subtitle: Text(
-                      'Os Simpsons '
-                      ,textAlign: TextAlign.center,),
-                    leading: CircleAvatar(backgroundImage: NetworkImage('https://media.fstatic.com/lKCjH5bY9N2lqLxX3JzhEnw_wFk=/290x478/smart/media/movies/covers/2019/08/2019-08-07_1.png')),
-                    onTap: () async {
-
-
-                      var navigationResult= await Navigator.push(context, new MaterialPageRoute(builder: (context) =>
-                          ChewieListItem(videoPlayerController: VideoPlayerController.network('https://videotemplate.lazyclique.com.br/Videos/Simpsons/ss.mp4'),looping: true)));
-
-                    },
-                    trailing: CircleAvatar(child: Icon(Icons.forward),),
-
-                  ),
-                ),
-                Card(
-                  child: ListTile(
-                    title: Text('Temporada 1',textAlign: TextAlign.center),
-                    subtitle: Text(
-                      'Os Simpsons '
-                      ,textAlign: TextAlign.center,),
-                    leading: CircleAvatar(backgroundImage: NetworkImage('https://media.fstatic.com/lKCjH5bY9N2lqLxX3JzhEnw_wFk=/290x478/smart/media/movies/covers/2019/08/2019-08-07_1.png')),
-                    onTap: () async {
-
-
-                      var navigationResult= await Navigator.push(context, new MaterialPageRoute(builder: (context) =>
-                          ChewieListItem(videoPlayerController: VideoPlayerController.network('https://videotemplate.lazyclique.com.br/Videos/Simpsons/ss.mp4'),looping: true)));
-
-                    },
-                    trailing: CircleAvatar(child: Icon(Icons.forward),),
-
-                  ),
-                ),
-                Card(
-                  child: ListTile(
-                    title: Text('Temporada 1',textAlign: TextAlign.center),
-                    subtitle: Text(
-                      'Os Simpsons '
-                      ,textAlign: TextAlign.center,),
-                    leading: CircleAvatar(backgroundImage: NetworkImage('https://media.fstatic.com/lKCjH5bY9N2lqLxX3JzhEnw_wFk=/290x478/smart/media/movies/covers/2019/08/2019-08-07_1.png')),
-                    onTap: () async {
-
-
-                      var navigationResult= await Navigator.push(context, new MaterialPageRoute(builder: (context) =>
-                          ChewieListItem(videoPlayerController: VideoPlayerController.network('https://videotemplate.lazyclique.com.br/Videos/Simpsons/ss.mp4'),looping: true)));
-
-                    },
-                    trailing: CircleAvatar(child: Icon(Icons.forward),),
-
-                  ),
-                ),
-                Card(
-                  child: ListTile(
-                    title: Text('Temporada 1',textAlign: TextAlign.center),
-                    subtitle: Text(
-                      'Os Simpsons '
-                      ,textAlign: TextAlign.center,),
-                    leading: CircleAvatar(backgroundImage: NetworkImage('https://media.fstatic.com/lKCjH5bY9N2lqLxX3JzhEnw_wFk=/290x478/smart/media/movies/covers/2019/08/2019-08-07_1.png')),
-                    onTap: () async {
-
-
-                      var navigationResult= await Navigator.push(context, new MaterialPageRoute(builder: (context) =>
-                          ChewieListItem(videoPlayerController: VideoPlayerController.network('https://videotemplate.lazyclique.com.br/Videos/Simpsons/ss.mp4'),looping: true)));
-
-                    },
-                    trailing: CircleAvatar(child: Icon(Icons.forward),),
-
-                  ),
-                ),
-                Card(
-                  child: ListTile(
-                    title: Text('Temporada 1',textAlign: TextAlign.center),
-                    subtitle: Text(
-                      'Os Simpsons '
-                      ,textAlign: TextAlign.center,),
-                    leading: CircleAvatar(backgroundImage: NetworkImage('https://media.fstatic.com/lKCjH5bY9N2lqLxX3JzhEnw_wFk=/290x478/smart/media/movies/covers/2019/08/2019-08-07_1.png')),
-                    onTap: () async {
-
-
-                      var navigationResult= await Navigator.push(context, new MaterialPageRoute(builder: (context) =>
-                          ChewieListItem(videoPlayerController: VideoPlayerController.network('https://videotemplate.lazyclique.com.br/Videos/Simpsons/ss.mp4'),looping: true)));
-
-                    },
-                    trailing: CircleAvatar(child: Icon(Icons.forward),),
-
-                  ),
-                ),
-                Card(
-                  child: ListTile(
-                    title: Text('Temporada 1',textAlign: TextAlign.center),
-                    subtitle: Text(
-                      'Os Simpsons '
-                      ,textAlign: TextAlign.center,),
-                    leading: CircleAvatar(backgroundImage: NetworkImage('https://media.fstatic.com/lKCjH5bY9N2lqLxX3JzhEnw_wFk=/290x478/smart/media/movies/covers/2019/08/2019-08-07_1.png')),
-                    onTap: () async {
-
-
-                      var navigationResult= await Navigator.push(context, new MaterialPageRoute(builder: (context) =>
-                          ChewieListItem(videoPlayerController: VideoPlayerController.network('https://videotemplate.lazyclique.com.br/Videos/Simpsons/ss.mp4'),looping: true)));
-
-                    },
-                    trailing: CircleAvatar(child: Icon(Icons.forward),),
-
-                  ),
-                ),
-
 
 
 
